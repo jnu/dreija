915fe8043127a051f7c14c36c3e0ac70ddb93899
Joseph Nudell <joenudell@gmail.com>
2013-08-07 18:49:46 -0500

    revised sort and added lorem-ipsum placeholder generator

--- js/config.js
+++ js/config.js
@@ -15,6 +15,7 @@ function() {
         "banner": "/img/banner.png",
         "bannerEl": "#banner",
         "bannerAlt": "",
+        "sideBarEl": "#sidebar",
         "headerEl": "#header",
         "footer": "/img/footer.png",
         "footerEl": "#footer",
@@ -62,9 +63,15 @@ function() {
         ],
         "homeEl": "#home",
         comparator: function(item) {
-            var c = item.get('category') || 'zzzzzzzzzzzzz';
-            return [c, item.get('created')];
+            var c = item.get('nothing!', false) || 'zzzzzzzzzzzzz',
+                d = new Date(item.get('created', 0)).valueOf();
+            return -d;
         },
+        Lorem: function(s) {
+            s = s||500;
+            var l = "Lorem ipsum dolor sit amet, id est nominavi qualisque disputationi, rebum democritum has et, summo quaestio no eum. Commodo indoctum te duo, duo an qualisque signiferumque, mea et democritum moderatius adversarium. Sea an suas lucilius, vocibus suscipiantur ad mei. Vidit discere vivendo id sit, ex errem ceteros pericula sed, vim option viderer ad. In est eirmod ocurreret. Dicta graeci eleifend ut est, ea quando audiam vidisse per. Nec doming labores et, sea eu probo eruditi tibique. Ne ipsum diceret sit, ius no eripuit facilis. Ex quodsi erroribus est, elitr solet per at. Adipisci postulant disputando id per. At nam harum errem reprehendunt, mei ei moderatius neglegentur. Quem vide ei per, ludus perpetua mea ut. In vis affert periculis vituperata, ea vocent oblique qui, usu error salutandi temporibus ad. Solet definiebas vix cu. Est ne homero audire minimum, dicam accumsan interesset no eam. Quo sensibus torquatos concludaturque ex, ut affert vituperata philosophia duo. Ea illum harum vel, et mutat nominavi legendos cum. Eu quot dissentias sit. Usu case gloriatur ad, his lorem essent fuisset te. Sed an sint nominavi. Nam cu iusto indoctum, nec cu quas omnes postea. Vis ne tota affert dicunt. Duo ne dicta vulputate, ea liber tamquam virtute eum.";
+            return l.substring(0, s);
+        }
     };
 
     return c;
