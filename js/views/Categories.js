172bc2c556287f564e390fc375b6b23c0109afd6
Joseph Nudell <joenudell@gmail.com>
2013-08-07 22:25:18 -0500

    added categories sidebar module

---
+++ js/views/Categories.js
@@ -0,0 +1,55 @@
+define([
+    'config',
+    'jquery',
+    'backbone'
+],
+function(Config, $, Backbone) {
+
+    var v = Backbone.View.extend({
+        id: 'module', // note -- modules are given UID when attached to SideBar
+        className: "pull-left cleared hidden-phone",
+        //
+        categories: [],
+        //
+        initialize : function(attrs) {
+            var that = this;
+
+            _.bindAll(this, 'render', 'makeCategoriesList');
+
+            this.makeCategoriesList();
+        },
+        //
+        makeCategoriesList : function() {
+            var c = _.uniq(this.collection.pluck('category')).sort();
+
+            this.categories = _.map(c, function(cat) {
+                var cats = cat.split(">"),
+                    subPath = _.map(cats, Blog.wp.cleanPostTitle).join("/");
+                cat = cats[cats.length-1];
+
+                return {
+                    depth: cats.length-1,
+                    label: cat,
+                    link: '/category/'+ subPath
+                }
+            });
+        },
+        //
+        render: function() {
+            var module = Blog.render('categories', {
+                categories: this.categories
+            });
+            this.$el.html(module);
+
+            return this;
+        },
+        //
+        remove: function() {
+            Backbone.View.prototype.remove.apply(this, arguments);
+            _.each(this.children, function(v){ v.remove(); });
+        }
+    });
+
+    return v;
+
+});
