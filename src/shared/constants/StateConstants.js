a8676e6797457c901ab42032cfb80f8b116acac3
Joe Nudell <joen@dropbox.com>
2016-02-04 23:43:05 -0800

    delete old code

--- src/shared/constants/StateConstants.js
+++
@@ -1,3 +0,0 @@
-export const LOAD_PAGE = 'LOAD_PAGE';
-export const LOAD_PAGE_SUCCESS = 'LOAD_PAGE:success';
-export const LOAD_PAGE_FAIL = 'LOAD_PAGE:fail';
--- src/shared/ui/About.jsx
+++
@@ -1,9 +0,0 @@
-/**
- * About page
- */
-
-import { createStaticPage } from 'ui/Factories';
-
-const ABOUT_RESOURCE_ID = 'about';
-
-export default createStaticPage(ABOUT_RESOURCE_ID);
