bc546c6a3723e65d71f9004eddd683f7f26869b8
Joe Nudell <joen@dropbox.com>
2016-02-20 13:14:17 -0800

    use env var for couchdb root

--- src/server/config.js
+++ src/server/config.js
@@ -1,7 +1,15 @@
 /**
- * CouchDB root
+ * DB Host name. Set this with an environment variable to point to couch
+ * running somewhere other than localhost. E.g. use docker-machine ip default
+ * for Couch running in a container through toolbox on OSX.
+ * @constant {string}
  */
-export const DB_HOST = 'http://localhost:5984';
+export const DBHOSTNAME = process.env.DBHOSTNAME || 'localhost';
+
+/**
+ * CouchDB root. Uses the DBHOSTNAME env variable.
+ */
+export const DB_HOST = `http://${DBHOSTNAME}:5984`;
 
 /**
  * CouchDB database id
