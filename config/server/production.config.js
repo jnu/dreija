6fd3c1ab39445d5187a23ab7a3224339f64b373f
Joe Nudell <joen@dropbox.com>
2016-10-18 21:32:29 -0700

    fix server build

--- config/server/production.config.js
+++ config/server/production.config.js
@@ -1,12 +1,12 @@
 var UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
 var config = require('./common.config');
 
-config.plugins.push(
-    new UglifyJsPlugin({
-        mangle: {
-            except: ['exports', 'module', 'require']
-        }
-    })
-);
+// config.plugins.push(
+//     new UglifyJsPlugin({
+//         // mangle: {
+//         //     except: ['exports', 'module', 'require']
+//         // }
+//     })
+// );
 
 module.exports = config;
